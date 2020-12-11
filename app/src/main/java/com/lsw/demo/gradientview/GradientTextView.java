d90999dc3fd523ec075a7e73be4d919bc2281471
liushuwei <liushuwei@kuaishou.com>
2020-12-11 18:14:50 +0800

    Initial commit

---
+++ app/src/main/java/com/lsw/demo/gradientview/GradientTextView.java
@@ -0,0 +1,39 @@
+package com.lsw.demo.gradientview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Create by liushuwei on 2020/12/8
+ */
+public class GradientTextView extends TextView {
+
+    private LinearGradient mLinearGradient;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private Rect mTextBound = new Rect();
+    public GradientTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mViewWidth = getMeasuredWidth();
+        mPaint = getPaint();
+        String mTipText = getText().toString();
+        mPaint.getTextBounds(mTipText, 0, mTipText.length(), mTextBound);
+        mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0,
+                new int[] {Color.parseColor("#00F0FF"), Color.parseColor("#1976E9") },//颜色设定
+                null, Shader.TileMode.REPEAT);
+        mPaint.setShader(mLinearGradient);
+        canvas.drawText(mTipText, getMeasuredWidth() / 2 - mTextBound.width() / 2, getMeasuredHeight() / 2 +   mTextBound.height()/2, mPaint);
+    }
+
+}
